7da7cfac84081bd6da9d8476f958b6b75705f894
gopikrishna <208r5a0510@gmail.com>
2025-12-23 23:03:25 +0530

    local

--- src/main/java/com/example/Employee_Be/controllers/EmployeeController.java
+++ src/main/java/com/example/Employee_Be/controllers/EmployeeController.java
@@ -10,8 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @Validated
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://gopikrishna2707.github.io")
+@CrossOrigin(origins = {"https://gopikrishna2707.github.io", "http://localhost:4200"})
 public class EmployeeController {
 
     EmployeeServiceImpl employeeService;
